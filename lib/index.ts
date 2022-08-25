67a50ccc2d84ed0892f77a1fc55f0704aa3da5d8
Malanius Privierre <malaniusprivierre@gmail.com>
2022-08-25 13:06:44 +0200

    Update handler runtime to Node14.x

--- lib/index.ts
+++ lib/index.ts
@@ -20,7 +20,7 @@ export class VerifiedSESIdentity extends Construct {
     });
 
     const handler = new lambda.Function(this, 'VerificationHandler', {
-      runtime: lambda.Runtime.NODEJS_12_X,
+      runtime: lambda.Runtime.NODEJS_14_X,
       timeout: cdk.Duration.seconds(10),
       handler: 'index.onEvent',
       code: lambda.Code.fromAsset(path.join(__dirname, '../handler')),
